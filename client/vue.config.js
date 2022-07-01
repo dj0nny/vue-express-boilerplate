0dd42c8411ea8641fd836470fcf81571cffcd6f7
dj0nny <frank96990@gmail.com>
2022-07-01 12:19:53 +0200

    deploy test

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: path.resolve(__dirname, '../public'),
+  devServer: {
+    'api/': {
+      target: 'http://localhost:5656',
+    }
+  }
 });
--- src/app.js
+++ src/app.js
@@ -22,10 +22,7 @@ app.use('/api', api);
 app.use(errorHandler);
 
 if (process.env.NODE_ENV == 'production') {
-  app.use(express.static('client/dist'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-  });
+  app.use(express.static(`${__dirname}/public`));
 }
 
 
